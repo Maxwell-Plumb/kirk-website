a930757d3cf5f58cbc1c201806d981b6ce504e0b
Maxwell-Plumb <ivan@maxwellplumb.com>
2024-08-13 15:54:53 -0400

    Fixed Hero Banner

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,8 +9,8 @@ const HeroSection = () => {
     navigate('/contact')
   }
   return (
-    <div id="HeroSection" className="relative h-screen w-full overflow-hidden pt-4 pr-4 pl-2 pb-20">
-      <div className="relative h-full w-full rounded-3xl overflow-hidden">
+    <div id="HeroSection" className="relative w-full overflow-hidden p-4 pb-8 bg-black">
+      <div className="relative h-[calc(100vh-6rem)] w-full rounded-3xl overflow-hidden">
         {/* Background image with gradient overlay */}
         <div className="absolute inset-0 bg-cover bg-center" style={{backgroundImage: `url('https://plus.unsplash.com/premium_photo-1666299429593-4da062e5e341?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`}}>
           <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-800/70 to-transparent"></div>
