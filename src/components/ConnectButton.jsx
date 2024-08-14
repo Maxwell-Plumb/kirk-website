22f2eddb1e1c26e88d9b5146d27a3a761bc3e346
Maxwell-Plumb <ivan@maxwellplumb.com>
2024-08-14 11:13:38 -0400

    Further SEO optimization

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { motion, AnimatePresence } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion/dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const ConnectButton = () => {
   const [showButton, setShowButton] = useState(false);
