5496568ad8525097b7145cbc33ea0738766ab9d3
Maxwell-Plumb <ivan@maxwellplumb.com>
2024-08-14 11:37:01 -0400

    Fixing error

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion/dom';
+import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom'
 
 const HeroSection = () => {
